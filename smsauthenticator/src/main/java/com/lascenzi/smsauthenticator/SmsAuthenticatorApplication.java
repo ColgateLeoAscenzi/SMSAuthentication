c29324cb0c1acc39bb33f9ac579ce4f219ef0618
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-04-20 20:26:58 -0400

    added starter code

---
+++ smsauthenticator/src/main/java/com/lascenzi/smsauthenticator/SmsAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.lascenzi.smsauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ smsauthenticator/src/test/java/com/lascenzi/smsauthenticator/SmsAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lascenzi.smsauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
